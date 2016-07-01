fbfd8dd7722495c9fe8a641623e063c0c3616e11
Ben Cameron <ben.cameron@d3tech.net>
2016-07-01 11:59:22 -0500

    Graphics are now displaying

--- src/app/model/model.component.ts
+++ src/app/model/model.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { C360ContextService } from 'angular2-c360';
+import { C360ContextService, C360ViewerComponent } from 'angular2-c360';
 import { C360PropComponent } from '../c360-prop';
 
 @Component({
@@ -8,7 +8,7 @@ import { C360PropComponent } from '../c360-prop';
   templateUrl: 'model.component.html',
   styleUrls: ['model.component.css'],
   providers: [C360ContextService],
-  directives: [C360PropComponent]
+  directives: [C360PropComponent,C360ViewerComponent]
 })
 export class ModelComponent implements OnInit {
 
